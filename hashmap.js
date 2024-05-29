3a06dd7ca13de3517db225dd23ac71b4e0dacdac
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-05-29 21:28:02 +0000

    Add hashmap.js

---
+++ hashmap.js
@@ -0,0 +1,69 @@
+
+class Node {
+    constructor(key=null, value = null) {
+        this.key = key;
+        this.value = value;
+        this.nextNode = null;
+    }
+}
+
+class HashMap{
+    /* constructor(head=null){
+        this.head = head;
+    } */
+    arr = new Array(4);// if (4) means we want only 4 places or buckets
+
+    hash(key){
+        let hashCode = 0;
+        const primeNumber = 5;// any prime number
+        for (let i=0; i < key.length; i++){
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = hashCode % 16;// b/cos js unable to hold large numbers precisely.
+        }
+        return hashCode;
+    }
+
+    set(key, value){
+        const idx = this.hash(key);
+        //this.arr[idx] = value;
+        let node = new Node(key,value);
+        if (this.arr[idx]){
+            if (this.arr[idx].key == key) {
+                this.arr[idx].value = value;
+            } else {
+                this.arr[idx].nextNode = node;
+            }        
+        } else {
+            this.arr[idx] = node;
+        }
+    }
+
+    get(key){
+        const idx = this.hash(key);
+        if (!this.arr[idx]){return null;}
+
+        return this.arr[idx]; 
+    }
+
+    has(key){
+        const idx = this.hash(key);
+        if (this.arr[idx]){
+            if(this.arr[idx].key == key) {
+                return true;
+            } else {return false;}
+        } else {
+            return false;
+        }
+    }
+
+}
+
+const myTable = new HashMap();
+myTable.set("lerf","opl"); myTable.set("kierf",5); myTable.set("lerf","neW");
+myTable.set(324,90); myTable.set("nased","likn"); myTable.set("nemd","absc"); 
+myTable.set("awsed","wde34frg"); 
+
+console.log(myTable.get("lerf")); console.log(myTable.get("kierf"));
+console.log(myTable.get(324));console.log(myTable.get("ik23"));
+console.log(myTable.has("ik23")); console.log(myTable.has("awsed"));
+console.log(myTable.arr);
