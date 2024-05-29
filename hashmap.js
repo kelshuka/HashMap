6bea9f6da69c7acf4b76d4d7182cb0cf020f51c4
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-05-29 22:09:41 +0000

    Add hashmap.js

--- hashmap.js
+++ hashmap.js
@@ -55,6 +55,54 @@ class HashMap{
             return false;
         }
     }
+    remove(key){
+        const idx = this.hash(key);
+        if (this.arr[idx]){
+            this.arr[key] = null;
+            return true;
+        } else {return false;}
+    }
+    length(){
+        let count = 0;
+        for (let ent of this.arr){
+            if (ent) {
+                count++;
+            }
+        }
+        return count;
+    }
+    clear(){
+        this.arr = null;
+    }
+    keys(){
+        const kArr = [];
+        for (let ent of this.arr){
+            if (ent){
+                kArr.push(ent.key);
+            }
+        }
+        return kArr;
+    }
+    values(){
+        const kArr = [];
+        for (let ent of this.arr){
+            if (ent){
+                kArr.push(ent.value);
+            }
+        }
+        return kArr;
+    }
+    entries(){
+        const kArr = [];
+        for (let ent of this.arr){
+            if (ent){
+                kArr.push([ent.key, ent.value]);
+            }
+        }
+        return kArr;
+
+    }
+
 
 }
 
@@ -63,7 +111,10 @@ myTable.set("lerf","opl"); myTable.set("kierf",5); myTable.set("lerf","neW");
 myTable.set(324,90); myTable.set("nased","likn"); myTable.set("nemd","absc"); 
 myTable.set("awsed","wde34frg"); 
 
-console.log(myTable.get("lerf")); console.log(myTable.get("kierf"));
-console.log(myTable.get(324));console.log(myTable.get("ik23"));
-console.log(myTable.has("ik23")); console.log(myTable.has("awsed"));
+//console.log(myTable.get("lerf")); console.log(myTable.get("kierf"));
+//console.log(myTable.get(324));console.log(myTable.get("ik23"));
+//console.log(myTable.has("ik23")); console.log(myTable.has("awsed"));
+//console.log(myTable.remove("kierf")); console.log(myTable.length());//myTable.clear();
+console.log(myTable.keys());console.log(myTable.values());
+console.log(myTable.entries());
 console.log(myTable.arr);
